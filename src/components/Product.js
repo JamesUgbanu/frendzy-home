37cfec3f8e49953c0d5a9d59232f6ce0983f2dc8
a <jamesugbanu@gmail.com>
2021-09-14 16:58:51 +0100

    update youtube embed link

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,7 +5,7 @@ export default function Product() {
     <div className="py-12 bg-red-600 overflow-hidden">
       <div className="grid md:grid-cols-2 mx-auto">
         <div className="relative md:ml-10 mx-5">
-          <YoutubeEmbed embedId="88vG7IRG03M" />
+          <YoutubeEmbed embedId="3J1rIRM1vwY" />
         </div>
         <div className="mt-5 max-w-7xl sm:mt-12 sm:px-6 md:mt-6 lg:mt-10 lg:px-8 xl:mt-5">
           <p className="text-base text-gray-200 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
